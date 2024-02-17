84a34c4faf1463c41a7ee1f9f0e67023ed957bba
Karlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2024-02-17 16:48:31 -0500

    form creation and injecting records to the database

--- src/app/componetns/customer-add/customer-add.component.ts
+++ src/app/componetns/customer-add/customer-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from 'src/app/Service/customer.service';
+import { Customer } from 'src/app/customer';
 
 @Component({
   selector: 'app-customer-add',
@@ -6,11 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-add.component.css']
 })
 export class CustomerAddComponent implements OnInit{
-  id : string ='';
+  id : number =0;
   firstName : string = '';
   lastName : string = '';
   email : string = '';
+
+  constructor(private customerService : CustomerService){
+
+  }
+
   ngOnInit(): void {
+  }
+
+  addCustomer(){
+    let customer = new Customer(this.id, this.firstName, this.lastName, this.email);
+
+    console.log(customer);
+    this.customerService.createCustomer(customer).subscribe(
+      res => console.log(res)
+    );
   }
 
 }
